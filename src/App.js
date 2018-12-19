5b95d8a0c0ff96a606124ecb8e18d42488f888ad
ahgCoding <805024102@qq.com>
2018-12-19 15:44:19 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header styleName="App-header">
+                    <img src={logo} styleName="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        styleName="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
